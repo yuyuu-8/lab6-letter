59548e4b31234920ad2e2c77039183757fca054e
yuyu <sishichulu@gmail.com>
2024-12-18 04:22:14 +0300

    init: start of project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useRef, useEffect } from 'react';
+import './App.css';
+
+
+const App = () => {
+
+  return (
+    <div className="app">
+      
+    </div>
+  );
+};
+
+export default App;
